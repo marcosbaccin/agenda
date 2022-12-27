e75cfb6850b53a671fe8cf6e397e480b71f9fa49
Marcos Baccin <marcos.baccin@estudante.ifgoiano.edu.br>
2022-12-27 12:45:20 -0300

    Criação de uma rota que retorna o local do evento através do nome do evento

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from core.models import Evento
 
 # Create your views here.
+
+def local(request, titulo_evento):
+    return HttpResponse(Evento.objects.get(titulo=titulo_evento).local)
