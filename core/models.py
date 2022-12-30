1a164160576907c61e0d14fb4580140d444fe670
Marcos Baccin <marcos.baccin@estudante.ifgoiano.edu.br>
2022-12-30 11:44:33 -0300

    Exercício para incluir o campo local no template de inserção

--- core/models.py
+++ core/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 
 class Evento(models.Model):
     titulo = models.CharField(max_length=100)
-    local = models.CharField(max_length=100)
+    local = models.CharField(max_length=100, blank=True, null=True)
     descricao = models.TextField(blank=True, null=True)
     data_evento = models.DateTimeField(verbose_name='Data do Evento')
     data_criacao = models.DateField(auto_now=True)
